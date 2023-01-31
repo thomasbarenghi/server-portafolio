4b461b8888e7d2ee09a3928ddfcc5ada024d3603
unknown <thomasbarenghi@gmail.com>
2023-01-31 18:43:01 -0300

    datbase

---
+++ src/api/portfolio-post/controllers/portfolio-post.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * portfolio-post controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::portfolio-post.portfolio-post');
---
+++ src/api/portfolio-post/routes/portfolio-post.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * portfolio-post router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::portfolio-post.portfolio-post');
